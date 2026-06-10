7d8c47cf6a9901de2e11d43dce4e9c539c6478cc
Ronin619 <eytam619@gmail.com>
2026-06-09 22:05:29 -0400

    implement controllers folder

---
+++ backend/controllers/authController.js
@@ -0,0 +1,16 @@
+import pool from "../db/db.js";
+import bcrypt from "bcrypt";
+
+export const signUp = async (req, res) => {
+  const { email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const data = "INSERT INTO users(email, password) VALUES($1, $2) RETURNING *";
+  try {
+    const result = await pool.query(data, [email, hashedPassword]);
+    return res.status(200).json({ success: true, user: result.rows[0] });
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to create user" });
+  }
+};
