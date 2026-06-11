ab9e99f226340d09995e0f7d783d8659ee5d4ba4
Ronin619 <eytam619@gmail.com>
2026-06-10 21:27:32 -0400

    implement passport middleware

---
+++ backend/middleware/protect.js
@@ -0,0 +1,35 @@
+import jwt from "jwt-simple";
+import pool from "../db/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const jwtSecret = process.env.JWT_SECRET;
+
+const protect = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "header is undefined" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  try {
+    const payload = jwt.decode(token, jwtSecret);
+    const data = "SELECT * FROM users WHERE id = $1";
+    const result = await pool.query(data, [payload.id]);
+    const user = result.rows[0];
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log("jwt error:", err);
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+export default protect;
