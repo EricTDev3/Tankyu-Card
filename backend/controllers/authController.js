8d410a69898a7edc18a8eae4d1533ce2476f3812
Ronin619 <eytam619@gmail.com>
2026-06-10 21:26:11 -0400

    update signUp route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,6 +7,7 @@ export const signUp = async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const data = "INSERT INTO users(email, password) VALUES($1, $2) RETURNING *";
+
   try {
     const result = await pool.query(data, [email, hashedPassword]);
     return res.status(200).json({ success: true, user: result.rows[0] });
