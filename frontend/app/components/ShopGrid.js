b16616cab90149256f3007b30abe468ef279afdc
Ronin619 <erictam.dev@gmail.com>
2026-07-05 22:45:30 -0400

    implement shop grid

---
+++ frontend/app/components/ShopGrid.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function ShopGrid() {
+  const [shops, setShops] = useState([]);
+
+  useEffect(() => {
+    const fetchShops = async () => {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/shops/getAllShops`,
+        { withCredentials: true },
+      );
+      setShops(response.data.shops);
+    };
+    fetchShops();
+  }, []);
+
+  console.log(shops);
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 ml-[20rem]">
+      {shops.map((shop) => (
+        <div key={shop.id}>
+          <a
+            href="#"
+            className="block border-2 border-black bg-white p-4 text-black shadow-[4px_4px_0_0] shadow-black hover:bg-yellow-200 focus:ring-2 focus:ring-yellow-300 focus:outline-0 sm:p-6"
+          >
+            <h3 className="mt-1 text-xl font-semibold">{shop.shop_name}</h3>
+
+            <p className="mt-2 line-clamp-2 text-pretty">
+              <span className="font-bold">Address:</span> {shop.address}
+            </p>
+            <p className="mt-2 line-clamp-2 text-pretty">
+              <span className="font-bold">TCG Sets sold:</span>{" "}
+              {shop.card_set_sold.map((set, index) =>
+                index === shop.card_set_sold.length - 1 ? `${set}` : `${set}, `,
+              )}
+            </p>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
