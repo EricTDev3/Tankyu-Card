227d4e36a0356c79845080ce05b0b8694cc5bbc6
Ronin619 <erictam.dev@gmail.com>
2026-07-07 18:27:48 -0400

    update cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "tankyu-card.vercel.app"],
     credentials: true,
   }),
 );
